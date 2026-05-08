6421552fd749baab423fa26dea136a98eb98b2a7
Gerald Versluis <gerald@verslu.is>
2026-05-08 14:59:12 +0200

    Apply suggestion from @jfversluis

--- src/Plugin.Maui.CalendarStore/CalendarStore.macios.cs
+++ src/Plugin.Maui.CalendarStore/CalendarStore.macios.cs
@@ -386,7 +386,7 @@ partial class CalendarStoreImplementation : ICalendarStore
 	}
 
 	static Calendar ToCalendar(EKCalendar calendar) =>
-		new(calendar.CalendarIdentifier, calendar.Source.Title, calendar.Title,
+		new(calendar.CalendarIdentifier, calendar.Source?.Title ?? string.Empty, calendar.Title,
 			new UIColor(calendar.CGColor).AsColor(),
 			!calendar.AllowsContentModifications);
 
